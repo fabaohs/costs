e9785839529f3b766c40666c648417561d453723
Fabio <fabiohenriquesf8@gmail.com>
2022-06-05 17:41:43 -0300

    Added Json Server and finished the post of project

--- src/components/Project/ProjectForm.jsx
+++ src/components/Project/ProjectForm.jsx
@@ -1,15 +1,54 @@
+import { useEffect, useState } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-export default function ProjectForm({ btnText }) {
+export default function ProjectForm({ handleSubmit, btnText, projectData }) {
+
+   const [categories, setCategories] = useState([])
+   const [project, setProject] = useState(projectData || [])
+
+   useEffect(() => {
+      fetch("http://localhost:5000/categories", {
+         method: "GET",
+         headers: {
+            'Content-Type': 'application/json'
+         }
+      }).then(res => res.json())
+         .then(data => {
+            setCategories(data)
+         })
+         .catch(error => console.log(error))
+   }, [])
+
+   const submit = (e => {
+      e.preventDefault()
+      handleSubmit(project)
+      
+   })
+
+   function handleChange(e) {
+      setProject({ ...project, [e.target.name]: e.target.value })
+   }
+
+   function handleSelect(e) {
+      setProject({
+         ...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text
+         }
+      })
+   }
 
    return (
-      <form className={styles.form}>
-         <Input type="text" text="Nome do projeto" name="name" placeholder="Digite o nome do projeto" />
-         <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Digite o orçamento do projeto" />
-         <Select name="category_id" text="Selecione a categoria" />
+      <form onSubmit={submit} className={styles.form}>
+         <Input type="text" text="Nome do projeto" name="name" placeholder="Digite o nome do projeto" handleOnChange={handleChange} value={project.name ? project.name : ''} />
+
+         <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Digite o orçamento do projeto" handleOnChange={handleChange} value={project.budget ? project.budget : ''} />
+
+         <Select name="category_id" text="Selecione a categoria" options={categories} handleOnChange={handleSelect} value={project.category ? project.category.id : ''} />
+
          <SubmitButton text={btnText} />
       </form>
    )
--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,13 +1,37 @@
 import ProjectForm from '../Project/ProjectForm'
 import styles from './NewProject.module.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function NewProject() {
+
+   const navigate = useNavigate()
+
+   function createPost(project) {
+
+      // fields
+      project.cost = 0
+      project.services = []
+
+      fetch("http://localhost:5000/projects", {
+         method: 'POST',
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify(project)
+
+      }).then(res => res.json())
+         .then(data => {
+            navigate('/projects')
+         })
+         .catch(err => console.log(err))
+
+   }
 
    return (
       <div className={styles.newproject_container}>
          <h1>Criar projeto</h1>
          <p>Crie um projeto para depois adicionar os serviços</p>
-         <ProjectForm btnText="Criar projeto" />
+         <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
       </div>
    )
 
